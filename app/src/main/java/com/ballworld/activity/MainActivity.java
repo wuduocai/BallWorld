f95099c213a9e9197144ad4e1f3d871a73fee820
N4A <181387406@qq.com>
2015-12-10 21:19:06 +0800

    修改分享模型

--- app/src/main/java/com/ballworld/activity/MainActivity.java
+++ app/src/main/java/com/ballworld/activity/MainActivity.java
@@ -591,19 +591,19 @@ public class MainActivity extends Activity {
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
         oks.setTitle("小球世界");
         // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://www.baidu.com");
+        oks.setTitleUrl("http://139.129.26.126:8080/hello/");
         // text是分享文本，所有平台都需要这个字段
         oks.setText("我是分享文本");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
+        oks.setUrl("http://139.129.26.126:8080/hello/");
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
         oks.setComment("我是测试评论文本");
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
+        oks.setSiteUrl("http://139.129.26.126:8080/hello/");
         // 启动分享GUI
         oks.show(this);
     }
